72fd709b5dc9a6ed559c44b68531f2e60e38dfc2
OhSeungSe <39735663+Mango-Coconut@users.noreply.github.com>
2025-08-28 14:00:48 +0900

    250828

--- Assets/0. Scripts/PlayerController.cs
+++ Assets/0. Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float turnSpeed = 180;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        gameObject.transform.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
+        gameObject.transform.Rotate(Vector3.up * r * turnSpeed * Time.deltaTime);
     }
 }
