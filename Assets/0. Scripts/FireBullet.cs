6742f8a54bdbf5f883fb52dfbf18047a53f23981
OhSeungSe <39735663+Mango-Coconut@users.noreply.github.com>
2025-09-05 14:38:47 +0900

    250905

---
+++ Assets/0. Scripts/FireBullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    [SerializeField] private GameObject Bullet;
+    [SerializeField] private Transform firePos;
+    private float delay = 0.1f;
+    private float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > delay && Input.GetMouseButton(0))
+        {
+            timer = 0;
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        Instantiate(Bullet, firePos.position, firePos.rotation);
+    }
+}
