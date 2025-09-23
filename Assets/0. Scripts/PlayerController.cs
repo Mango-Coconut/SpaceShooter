9b25b61f1eb10574164dbe89674fbcf3a4b567b3
OhSeungSe <39735663+Mango-Coconut@users.noreply.github.com>
2025-09-23 10:44:18 +0900

    Player Animation legacy -> mechanim

--- Assets/0. Scripts/PlayerController.cs
+++ Assets/0. Scripts/PlayerController.cs
@@ -8,16 +8,15 @@ public class PlayerController : MonoBehaviour
     bool ispick = false;
     [SerializeField] float moveSpeed = 5;
     [SerializeField] float turnSpeed = 360;
-    Animation anim;
+
+    Animator animator;
     void Awake()
     {
-        anim = GetComponent<Animation>();
+        animator = GetComponent<Animator>();
     }
 
     IEnumerator Start()
     {
-        anim.Play("Idle");
-
         turnSpeed = 0f;
         yield return new WaitForSeconds(0.3f);
         turnSpeed = 360.0f;
@@ -28,43 +27,9 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         float r = Input.GetAxis("Mouse X");
-        PlayerMoveAnim(h, v);
         if (ispick) return;
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         gameObject.transform.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
         gameObject.transform.Rotate(Vector3.up * r * turnSpeed * Time.deltaTime);
-    }
-    void PlayerMoveAnim(float h, float v)
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            ispick = true;
-            anim.CrossFade("Picking Up", 0.25f);
-        }
-        if (ispick) return;
-        if (v >= 0.1f)
-        {
-            anim.CrossFade("RunF", 0.25f);
-        }
-        else if (v < -0.1f)
-        {
-            anim.CrossFade("RunB", 0.25f);
-        }
-        else if (h >= 0.1f)
-        {
-            anim.CrossFade("RunR", 0.25f);
-        }
-        else if (h < -0.1f)
-        {
-            anim.CrossFade("RunL", 0.25f);
-        }
-        else
-        {
-            anim.CrossFade("Idle", 0.25f);
-        }
-    }
-    public void disablePickUp()
-    {
-        ispick = false;
     }
 }
